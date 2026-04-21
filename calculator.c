e39def16a0837bd95db4111b3b083e336e547a12
addiswebb <akwgma@gmail.com>
2026-04-21 14:41:13 +0100

    Separate into single file and double file versions for testing

--- calculator.c
+++ calculator.c
@@ -1,9 +1,52 @@
 
-#include "calculator.h"
-#include "stdio.h"
+/*
+        Written by: Addis Webb
+        Program to be a calculator?
+        Input: 5 * 5
+        Output: = 10;
+*/
+
 #include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#define DEBUG false
+
+enum ParseFlag {
+    EQUATION,
+    VARIABLE,
+    FUNCTION,
+};
+
+typedef enum {
+    FLOAT,
+    BOOL,
+    ERROR,
+} Type;
+
+typedef struct {
+    double value;
+    Type type;
+    long _p1;
+    long _p2;
+    long _p3;
+} Var;
+
+Var parse(char *input, int length, enum ParseFlag *parse_flag);
+void format(char *input);
+bool is_operator(char c);
+Var parseToken(char *input);
+Var solve(Var a, Var b, char operator);
+int op_priority(char op);
+bool isAlpha(char c);
+void printVar(Var v);
+void printError(char *str, char *token);
+void init_calculator();
+
+#define NUMBER_OF_CONSTANTS 3
+#define MAX_VARIABLES 20
 
 char *CONSTANTS_NAMES[NUMBER_OF_CONSTANTS];
 
